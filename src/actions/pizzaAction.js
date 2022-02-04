712a3c5f058125e940440b96dc13328b1988cd9b
DeepaThangaraju <deepat2405@gmail.com>
2022-02-04 13:37:21 +0530

    link

--- src/actions/pizzaAction.js
+++ src/actions/pizzaAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const getAllPizzas=()=>async dispatch=>{
     dispatch({type:'GET_PIZZAS_REQUEST'})
     try{
-        const response=await axios.get("/api/pizzas/getallpizzas");
+        const response=await axios.get("https://deepapizzadelivery.herokuapp.com/pizzas/getallpizzas");
         console.log(response);
         dispatch({type:'GET_PIZZAS_SUCCESS', payload:response.data})
     }catch(error){
